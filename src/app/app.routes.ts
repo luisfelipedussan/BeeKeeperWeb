f2d6bdc26b3109813030c37fe433f48a3cc3c65d
Alejandro Gómez Colorado <a.gomez65@uniandes.edu.co>
2024-03-13 23:32:50 -0500

    Pantallas main-page y recarga automática

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NavHomeComponent } from './components/nav-home/nav-home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { LoginComponent } from './components/login/login.component';
 import { RecargaComponent } from './components/recarga/recarga.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 
 
@@ -12,6 +13,7 @@ export const routes: Routes = [
     { title: "BM Home", path: 'Home' , component : LoginComponent },
     { title: "BM Contact", path: 'contact' , component : ContactComponent },
     { title: "BM Login", path: 'login' , component : LoginComponent },
-    { title: "BM Recarga", path: 'recarga' , component : RecargaComponent }
+    { title: "BM Main Page", path: 'main-page' , component : MainPageComponent},
+    { title: "BM Recarga", path: 'recargaApp' , component : RecargaComponent }
 
 ];
--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { Component , ChangeDetectionStrategy} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -17,18 +16,19 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatButtonToggleGroup } from '@angular/material/button-toggle';
-
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
 @Component({
-  selector: 'app-recarga',
+  selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive,
-    MatSidenavModule, MatToolbarModule, MatCardModule, MatIconModule, MatExpansionModule,
-    MatButtonToggleModule, MatButtonToggleGroup, MatGridListModule, MatInputModule,
-    MainNavComponent, FooterComponent],
-  templateUrl: './recarga.component.html',
-  styleUrl: './recarga.component.scss'
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive
+    , MatSidenavModule, MatToolbarModule, MatCardModule, MatIconModule, MatExpansionModule,
+     MatButtonToggleModule, MatButtonToggleGroup, MatGridListModule, MatInputModule, 
+     MainNavComponent, FooterComponent, ScrollingModule ],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.scss'
 })
-export class RecargaComponent {
+export class MainPageComponent {
 
 }
