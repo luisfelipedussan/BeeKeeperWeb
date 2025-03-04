362cb6ca8e391735e2a3c94204f108780f6d7d9d
Luis Felipe Dussán Rueda <78167781+luisfelipedussan@users.noreply.github.com>
2025-03-04 18:50:05 -0500

    fix deploy

--- bill-master-web-project/src/app/app-routing.module.ts
+++ bill-master-web-project/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/contact', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'contact', component: ContactComponent },
 ];
--- bill-master-web-project/src/app/app.module.ts
+++ bill-master-web-project/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    LoginComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
