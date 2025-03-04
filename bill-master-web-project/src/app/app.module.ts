95d15ff64c3fa02ff30d3999bab90698ef1e8808
Luis Felipe Dussán Rueda <78167781+luisfelipedussan@users.noreply.github.com>
2025-03-04 18:40:17 -0500

    fixes deploy

---
+++ bill-master-web-project/src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+})
+export class AppModule { } 
